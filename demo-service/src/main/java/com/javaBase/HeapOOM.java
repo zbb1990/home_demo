3d2551613a8c4617df910e4442ebb462df2758b1
zhangbinbin <rugao.1990@163.com>
2016-05-03 11:52:28 +0800

    first test upload to github

--- demo-service/src/main/java/com/javaBase/HeapOOM.java
+++ demo-service/src/main/java/com/javaBase/HeapOOM.java
@@ -15,6 +15,7 @@ public class HeapOOM {
     public static void main(String[] args) {
         List<OOMObject> list = new ArrayList<OOMObject>();
 
+
         while(true){
             list.add(new OOMObject());
         }
